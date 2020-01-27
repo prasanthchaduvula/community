31bcd4f650beb8e3cf5a8e97efdad6db4e3d432b
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-01-27 15:56:50 +0530

    removed repeated community names

--- src/components/Communities.jsx
+++ src/components/Communities.jsx
@@ -14,9 +14,34 @@ class Communities extends React.Component {
         { logo: 'altcampus/music.jpeg', name: 'Music' },
         { logo: 'altcampus/suraj_story.JPG ', name: 'Friends' },
         { logo: 'altcampus/cse.jpeg', name: 'Cse branch' }
-      ]
+      ],
+      allcommunities: [
+        { logo: '/altcampus/altcampus.jpg', name: 'Altcampus' },
+        { logo: '/upsc/upsc.jpeg', name: 'Upsc' },
+        { logo: '/travel/travel.JPG', name: 'Travel' },
+        { logo: 'altcampus/prasanth_story.JPG', name: 'My village' },
+        { logo: 'altcampus/ravi_story.JPG ', name: 'Family' },
+        { logo: 'altcampus/music.jpeg', name: 'Music' },
+        { logo: 'altcampus/suraj_story.JPG ', name: 'Friends' },
+        { logo: 'altcampus/cse.jpeg', name: 'Cse branch' }
+      ],
+      filterCommunity: ''
     };
   }
+
+  handleSearch = event => {
+    if (event.target.value) {
+      console.log('hi', event.target.value);
+      this.state.communities.map(community =>
+        community.name == event.target.value
+          ? this.setState({ communities: [community] })
+          : ''
+      );
+    } else {
+      this.setState({ communities: this.state.allcommunities });
+    }
+  };
+
   // handleClick = () => {
   //   this.setState({ show: true });
   //   if (this.state.show) {
@@ -29,6 +54,15 @@ class Communities extends React.Component {
     return (
       <>
         <div className="communities-section">
+          <div className="community-search-wrapper">
+            <input
+              className="community-search"
+              type="text"
+              name=""
+              placeholder="Search your community"
+              onChange={this.handleSearch}
+            />
+          </div>
           {/* <p className="communities-heading">Welcome to Communities</p> */}
           {communities.map(community => (
             <div
@@ -40,6 +74,7 @@ class Communities extends React.Component {
             </div>
           ))}
         </div>
+        <div className="post-btn">+</div>
       </>
     );
   }
